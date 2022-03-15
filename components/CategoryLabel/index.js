785f037f2f02f0d464bb5479ab3ed9288e70c406
mhmdhalawi <mohammadhalawi93@gmail.com>
2022-03-15 17:06:40 +0200

    add category label

---
+++ components/CategoryLabel/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function CategoryLabel({ children }) {
+  const colorKey = {
+    JavaScript: 'yellow',
+    CSS: 'blue',
+    PHP: 'purple',
+    Python: 'green',
+    Ruby: 'red',
+  };
+  const bgColor = `bg-${colorKey[children]}-600 `;
+  return (
+    <div className={`py-1 px-2 ${bgColor} text-gray-100 rounded font-bold `}>
+      <Link href={`/blog/catergory/${children.toLowerCase()}`}>{children}</Link>
+    </div>
+  );
+}
--- components/Post/Post.jsx
+++ components/Post/Post.jsx
@@ -2,8 +2,9 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import CategoryLabel from '../CategoryLabel';
+
 export default function Post({ post }) {
-  console.log(post);
   return (
     <div className='w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
       <Image
@@ -16,7 +17,7 @@ export default function Post({ post }) {
 
       <div className='flex justify-between items-center'>
         <span className='font-light text-gray-600'>{post.frontmatter.date}</span>
-        <div>{post.frontmatter.category}</div>
+        <CategoryLabel>{post.frontmatter.category}</CategoryLabel>
       </div>
       <div className='mt-4'>
         <Link href={`/blog/${post.slug}`}>
